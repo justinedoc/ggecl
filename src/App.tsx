18351a0e717f6348ae987e9e93f9ae0ab20c50fc
bi-Tech2 <jodicksonjoshua@gmail.com>
2025-02-08 02:21:48 +0100

    AllCategories UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import Home from "./components/landing/Home";
 import Login from "./components/auth/pages/Login";
 import Signup from "./components/auth/pages/Signup";
+import AllCourses from "./components/landing/_components/AllCourses";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/all-courses" element={<AllCourses />} />
       <Route path="*" element={<h1>Not found</h1>} />
     </Routes>
   );
